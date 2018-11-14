df7f7f02f5d266b0642b1ac2bcebb6cb34716d8a
TW <lfwutong@gmail.com>
2018-11-14 00:16:02 -0500

    noise added

---
+++ Codes/noise.py
@@ -0,0 +1,34 @@
+import numpy as np
+import pandas as pd
+import os
+import bind
+
+
+
+
+def gauss_noise(signal):
+    noise = np.random.normal(0, 0.000001, len(signal))
+    return noise+signal
+
+
+if __name__ == '__main__':
+    path = '/Users/wuxiaodong/Desktop/18fall/SpecialProblem/Rice/'
+    path_list = os.listdir(path)
+    path_list.sort()
+    name_list = path_list
+    d = len(path_list)
+
+    for i in range(d):
+        path_list[i] = path + path_list[i]
+        print path_list[i]
+
+    for filename in path_list:
+        raw_data = pd.read_csv(filename, names=['date', 'value'])
+
+        raw_data['date'] = pd.to_datetime(raw_data['date'], unit='s')
+        raw_data = raw_data.sort_values(by=['date'])
+        raw_value = raw_data['value']
+        if raw_value[0] == raw_value[1]:
+            new_value =gauss_noise(np.array(raw_value))
+            raw_data['value'] = new_value
+            raw_data.to_csv(filename)

c372c23d8d66f6bac116d6d0461707cd0fe85cb1
TW <lfwutong@gmail.com>
2018-12-02 23:10:10 -0500

    bug fixed

--- Codes/anomaly_count.py
+++ Codes/anomaly_count.py
@@ -10,11 +10,9 @@ path = '/Users/wuxiaodong/Dropbox/adaptive-anomalies/'
 # frequency range:
 f_range = 2
 
-# number of time bins:
-n = 4
-
 # constant b of MAD:
 b = 1.4826
+
 
 
 def read_Rmatrix(num_days):
@@ -25,16 +23,15 @@ def read_Rmatrix(num_days):
     return R
 
 
-def read_Cmatrix(set, timebin):
-    if timebin > n-1:
-        return 'error: timebin is out of range'
+def read_Cmatrix(set, day, timebin):
     c_path = path+'C/'
     path_list = os.listdir(c_path)
     path_list.sort()
     d = len(path_list)
     for i in range(d):
         path_list[i] = c_path + path_list[i]
-    df = pd.read_csv(path_list[timebin])
+    order = (day-1) % 4 + timebin
+    df = pd.read_csv(path_list[order])
     C = df.values
     C = C[:, 1:]
     return C
@@ -42,26 +39,40 @@ def read_Cmatrix(set, timebin):
 
 def get_Cmatrix_list(set):
     list = []
-    for i in range(n):
-        list.append(read_Cmatrix(set, i))
+    for day in range(1, 7):
+        for tb in range(0, 3):
+            list.append(read_Cmatrix(set, day, tb))
     return list
 
 
-def count(R, C_list, timebin, threshold):
+def count(R, C_list, day, timebin, threshold):
     R = np.array(R)
-    if len(C_list) != n:
-        return 'Error: C matrices are not enough.'
     sum = 0
+    tb = (day-1) % 4 + timebin
     for i in range(0, len(R)):
         for j in range(i+1, len(R)):
             l = []
-            for t in range(n):
-                l.append(search.behaviorChange(R, C_list[t], i, j))
+            for c_matrix in C_list:
+                l.append(search.behaviorChange(R, c_matrix, i, j))
             MAD = search.MAD(l, b)
-            if search.anomaly(l, MAD, threshold, timebin) is True:
+            if search.anomaly(l, MAD, threshold, tb) is True:
+                sum = sum + 1
+    return sum/2
+
+
+def detail(R, C_list, day, timebin, threshold):
+    R = np.array(R)
+    sum = 0
+    tb = (day-1) % 4 + timebin
+    for i in range(0, len(R)):
+        for j in range(i+1, len(R)):
+            l = []
+            for c_matrix in C_list:
+                l.append(search.behaviorChange(R, c_matrix, i, j))
+            MAD = search.MAD(l, b)
+            if search.anomaly(l, MAD, threshold, tb) is True:
                 sum = sum + 1
                 print 'anomaly is in: (' + str(i) + ', ' + str(j)+')'
-    print 'anomalies: ', sum
     return sum/2
 
 
@@ -81,6 +92,6 @@ def plot():
 
 
 if __name__ == '__main__':
-    R = read_Rmatrix(3)
+    R = read_Rmatrix(1)
     C_list = get_Cmatrix_list('Rice')
-    count(R, C_list, 3, 9)
+    count(R, C_list, 20, 20)

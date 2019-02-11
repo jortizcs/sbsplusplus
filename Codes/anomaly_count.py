0b28c3c501ce710a1ae62d7c7d2261d828504bd0
TW <lfwutong@gmail.com>
2019-02-11 10:33:54 -0500

    manually check count updated

--- Codes/anomaly_count.py
+++ Codes/anomaly_count.py
@@ -4,6 +4,7 @@ import bind
 import matplotlib.pyplot as plt
 import search
 import os
+import plot_graphs
 
 path = '/Users/wuxiaodong/Dropbox/adaptive-anomalies/'
 
@@ -18,7 +19,7 @@ def count_interface(R, C_list, day, timebin, threshold, option):
     if option == 1:
         return count_sp_day(R, C_list, day, timebin, threshold)
     else:
-        return count_abn_sensor(R, C_list, timebin, threshold)
+        return count_abn_sensor(R, C_list, day, timebin, threshold)
 
 
 def read_Rmatrix(num_days):
@@ -80,18 +81,55 @@ def count(R, C_list, timebin, threshold):
     return sum/2
 
 
-def count_abn_sensor(R, C_list, timebin, threshold):
+def count_abn_sensor(R, C_list, day, timebin, threshold):
     R = np.array(R)
-    sum = 0
+    abn_sensors = []
+    tb = (day-1)*4 + timebin
     for i in range(0, len(R)):
         l_i = []
         for c_matrix in C_list:
             l_i.append(search.behaviorChange_old(R, c_matrix, i))
         MAD = search.MAD(l_i, b)
+
         if search.anomaly(l_i, MAD, threshold, timebin) is True:
             print 'Abnormal behavior in sensor: ' + str(i)
-            sum = sum + 1
-    return sum
+            abn_sensors.append(i)
+    return abn_sensors
+
+
+def anomaly_check(R, abn_sensors, day, timebin):
+    control_sensors = []
+    correct = 0
+    false = 0
+    for sensor in abn_sensors:
+        R_array = np.array(R)
+        sensor_array = R_array[:, sensor]
+        control_sensor = find_second_largest_index(sensor_array, sensor)
+        control_sensors.append(control_sensor)
+    for i in range(len(abn_sensors)):
+        plot_graphs.plot_pair_by_id(abn_sensors[i], control_sensors[i], day, timebin)
+        plt.show()
+        print 'Press 1 for correct, 2 for false'
+        check = int(raw_input())
+        if check == 1:
+            correct +=1
+        else:
+            false +=1
+    print 'The correct anomalies are in total: '
+    print correct
+    print 'The false anomalies are in total: '
+    print false
+
+
+def find_second_largest_index(array, sensor):
+    second_largest = -1
+    index = 0
+    for i in range(len(array)):
+        if i != sensor:
+            if array[i] > second_largest:
+                second_largest = array[i]
+                index = i
+    return index
 
 
 def detail_cell(R, C_list, day, timebin, threshold):
@@ -126,6 +164,7 @@ def plot():
 
 
 if __name__ == '__main__':
-    R = read_Rmatrix(1)
-    C_list = get_Cmatrix_list('Rice')
-    count(R, C_list, 20, 20)
+    path = '/Users/wuxiaodong/Desktop/18fall/SpecialProblem/Rice/'
+    path_list = os.listdir(path)
+    path_list.sort()
+    print path_list

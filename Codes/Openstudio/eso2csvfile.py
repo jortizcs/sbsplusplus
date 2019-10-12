280fa716edf56c7685b34f2c0853bff70970366c
TW <lfwutong@gmail.com>
2019-10-12 17:58:59 -0400

    bug fixed

--- Codes/Openstudio/eso2csvfile.py
+++ Codes/Openstudio/eso2csvfile.py
@@ -2,7 +2,8 @@ import esoreader
 import pandas as pd
 import matplotlib.pyplot as plt
 
-ESO_path = "/Users/wuxiaodong/Dropbox/adaptive-anomalies/energyplus/anomaly/"
+ESO_path = "/Users/wuxiaodong/Dropbox/adaptive-anomalies/demo/eso/"
+CSV_path = "/Users/wuxiaodong/Dropbox/adaptive-anomalies/demo/csv/"
 
 
 def eso2csv(subpath, variable):
@@ -10,12 +11,15 @@ def eso2csv(subpath, variable):
     time_index = pd.date_range('2009-01-01', periods=1152, freq='10min')  # 10 mins for each timestamp
     df = eso.to_frame(variable, index=time_index)
     sensor_list = df.columns.values.tolist()
-    for sensor in sensor_list:
-        df[sensor].to_csv(ESO_path + variable + '_' + sensor + '.csv')
+    #for sensor in sensor_list:
+    sensor = sensor_list[0]
+    df[sensor].to_csv(CSV_path + variable + '_' + sensor + '.csv')
 
 
 variables = ['Lights Electric Energy', 'Electric Equipment Electric Energy', 'Zone Total Internal Total Heating Energy',
              'Zone Air Temperature', 'People Occupant Count']
 
+
 for variable in variables:
-    eso2csv('eplusout.eso', variable)
+    if variable == 'Lights Electric Energy':
+        eso2csv('eplusout.eso', variable)
--- Codes/Openstudio/sbs_detection.py
+++ Codes/Openstudio/sbs_detection.py
@@ -12,8 +12,8 @@ C_OUTPUT_PATH = '/Users/wuxiaodong/Dropbox/adaptive-anomalies/energyplus/anomaly
 
 sbs = SBS(R_PATH, C_PATH, FS, FREQUENCY_RANGE, TIMEBINS_PER_DAY, TOTAL_DAY, R_OUTPUT_PATH, C_OUTPUT_PATH)
 
-#sbs.R_generator()
+sbs.R_generator()
 #sbs.C_generator()
-for s in range(0, 15):
-    sbs.anomaly_detector(s, 9, 4, 1.4826, 1, 8)
+#for s in range(0, 15):
+    #sbs.anomaly_detector(s, 9, 4, 1.4826, 1, 8)
 

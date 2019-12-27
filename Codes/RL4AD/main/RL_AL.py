c8e03e9ffa3aa42d185a9c9176912373e726b490
TW <lfwutong@gmail.com>
2019-12-27 14:48:02 +0800

    test committed

--- Codes/RL4AD/main/RL_AL.py
+++ Codes/RL4AD/main/RL_AL.py
@@ -818,7 +818,7 @@ def train(num_LP, num_AL, discount_factor):
         # dataset_dir = ['/Users/wuxiaodong/Dropbox/adaptive-anomalies/demo/csv/']
         # dataset_dir = ['/Users/wuxiaodong/Dropbox/adaptive-anomalies/Aniyama_groundtruth/dataport/']
         # dataset_dir = ['/home/sciphilab/sbsplusplus/datasets/Aniyama_groundtruth/dataport/']
-        dataset_dir = ['/home/scifilab/anomaly_detection/dataset/A1Benchmark/']
+        #dataset_dir = ['/home/scifilab/anomaly_detection/dataset/A1Benchmark/']
         for i in range(len(dataset_dir)):
             env = EnvTimeSeriesfromRepo(dataset_dir[i])
             env.statefnc = RNNBinaryStateFuc
@@ -869,6 +869,3 @@ def train(num_LP, num_AL, discount_factor):
                            test=test)
                 optimization_metric = q_learning_validator(env_test, qlearn_estimator, 7, experiment_dir)
             return optimization_metric
-
-
-train(92, 5, 0.8)

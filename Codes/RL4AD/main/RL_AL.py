6268ec4e606980a575e442ed1b517b4de099775b
TW <lfwutong@gmail.com>
2020-01-04 20:15:23 +0800

    bug fixed

--- Codes/RL4AD/main/RL_AL.py
+++ Codes/RL4AD/main/RL_AL.py
@@ -820,7 +820,7 @@ def train(num_LP, num_AL, discount_factor):
         # dataset_dir = ['/home/sciphilab/sbsplusplus/datasets/Aniyama_groundtruth/dataport/']
         #dataset_dir = ['/home/scifilab/anomaly_detection/dataset/A1Benchmark/']
         #dataset_dir = ['/Users/wuxiaodong/Downloads/KPI_dataset']
-        dataset_dir = ['/home/scifilab/anomaly_detection/dataset/KPI/']
+        dataset_dir = ['/home/scifilab/anomaly_detection/dataset/KPI_dataset/']
         for i in range(len(dataset_dir)):
             env = EnvTimeSeriesfromRepo(dataset_dir[i])
             env.statefnc = RNNBinaryStateFuc
